ecfe134f029c710a3e431ad68d7b74b18d32d9dc
lakshminarasimha06 <lakshmi.narasimha@ekfrazo.in>
2026-01-07 19:15:05 +0530

    bASE URL

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://165.232.181.49:3001",
+  baseURL: import.meta.env.VITE_API_URL,
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
